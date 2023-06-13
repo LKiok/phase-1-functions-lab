bcc51d6a25ccbab57ddb552a436b6bac33af1565
LKiok <kiokletia720@gmail.com>
2023-06-13 12:55:58 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const Hq =42
+
+function distanceFromHqInBlocks (pickup){
+    let response = pickup - Hq;
+    if (response <0) {
+        return response * -1;
+    }
+    return response;
+}
+
+let ft =264
+function  distanceFromHqInFeet (distanceFeet){
+    return distanceFromHqInBlocks(distanceFeet)*ft;
+}
+
+
+function distanceTravelledInFeet(start,destination){
+    let response = (destination - start)*ft;
+    if (response<0) {
+        return response *-1
+    }
+    return response;
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceTravelled = distanceTravelledInFeet(start,destination);
+     if(distanceTravelled <= 400){
+        return 0;
+     }else if (distanceTravelled > 400 && distanceTravelled <= 2000){
+        return(distanceTravelled-400)* 0.02;l
+     }else if(distanceTravelled > 2000 && distanceTravelled <= 2500){
+        return 25;
+     }else if(distanceTravelled > 2500){
+        return 'cannot travel that far';
+     }
+}
+
+
+
+
+
+
